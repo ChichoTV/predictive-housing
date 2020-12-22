91f7f48cf3258fffa6d645182b17aaa1445faed5
RHARLESS <rharless99@gmail.com>
2020-12-21 18:30:25 -0800

    update mortage pages to copy and paste to templates and static folders

--- MortgagePage/mortgage.js
+++ MortgagePage/mortgage.js
@@ -1,39 +1,48 @@
-// var homePrice = $("#HomePrice").node.value()
 
-// console.log(homePrice)
-// var downPayment = $("#DownPayment").val();
-// var interestRate = $("#Interest").val();
-// var mortgagePeriod = getSelectedOption("SelectTerm")
-// console.log(mortgagePeriod);
-var homePrice = 150000
-console.log(homePrice)
-var downPayment = 12000
-var interestRate = 3.5
-var mortgagePeriod = 15
+// Hard coding practice values. 
+// var downPayment = 12000
+// var interestRate = 3.5
+// var mortgagePeriod = getSelectedOption(SelectTerm);
 
-// function MortgageCalculator(){
+function MortgageCalculator(){
+    let homePrice = $("#HomePrice").val()
+    let downPayment = $("#DownPayment").val();
+    let interestRate = $("#Interest").val();
+    let mortgagePeriod = formatOption();
     let i = (interestRate/100)/12;
     let t = mortgagePeriod*12;
     let Value = homePrice - downPayment;
     let mortgagePayment = (Value)*((i*((1+i)**t))/(((1+i)**t)-1));
-    // document.getElementById("Monthly_Mortgage").innerHTML=`The monthly mortgage payment is: ${mortagePayment}`;
-    console.log(mortgagePayment)
-    // return mortgagePayment;
+    let insurance = $("#HomeInsurance").val()/12
+    let taxes = (($("#PropertyTax").val()/100)* Value)/12
+    let MonthlyPayment = Math.round(mortgagePayment + insurance + taxes)
+    document.getElementById("Monthly_Mortgage").innerHTML=`The monthly mortgage payment is: ${MonthlyPayment}`;
+    console.log(MonthlyPayment)
+    return MonthlyPayment;
+}
+
+function formatOption(){
+    let term = getSelectedOption("SelectTerm");
+    let answer = term.value;
+    let finalAnswer = parseInt(answer)
+    console.log(term);
+    console.log(answer);
+    console.log(typeof answer);
+    console.log(typeof finalAnswer)
+    return finalAnswer
+}
+function getSelectedOption(selectID){
+    let htmlElement = document.getElementById(selectID);
+    for (let i = 0; i < htmlElement.options.length; i++){
+        let option = htmlElement.options[i];
+        if(option.selected == true){
+            return option;
+        }
+    }
+    return null
+}
 
 
-// }
-// MortgageCalculator();
-
-// function getSelectedOption(selectID){
-//     let htmlElement = document.getElementById(selectID);
-//     for (let i = 0; i < htmlElement.options.length; i++){
-//         let option = htmlElement.options[i];
-//         if(option.selected == true){
-//             return option;
-//         }
-//     }
-//     return null
-// }
 
 
-// d3.select("#MortgageCalc_Button").on("click", MortgageCalculator)
+d3.select("#MortgageCalc_Button").on("click", MortgageCalculator)
