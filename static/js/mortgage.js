ac2f20e2e021683cbfc4514001b4f52feae971a2
Taylor Vought <65677292+ChichoTV@users.noreply.github.com>
2020-12-22 13:13:28 -0800

    many fixes and navs

--- static/js/mortgage.js
+++ static/js/mortgage.js
@@ -1,39 +1,75 @@
-// var homePrice = $("#HomePrice").node.value()
 
-// console.log(homePrice)
-// var downPayment = $("#DownPayment").val();
-// var interestRate = $("#Interest").val();
-// var mortgagePeriod = getSelectedOption("SelectTerm")
-// console.log(mortgagePeriod);
-var homePrice = 150000
-console.log(homePrice)
-var downPayment = 12000
-var interestRate = 3.5
-var mortgagePeriod = 15
+// Hard coding practice values. 
+// var downPayment = 12000
+// var interestRate = 3.5
+// var mortgagePeriod = getSelectedOption(SelectTerm);
 
-// function MortgageCalculator(){
+function MortgageCalculator(){
+    let homePrice = $("#HomePrice").val()
+    let downPayment = $("#DownPayment").val();
+    let interestRate = $("#Interest").val();
+    let mortgagePeriod = formatOption();
     let i = (interestRate/100)/12;
     let t = mortgagePeriod*12;
     let Value = homePrice - downPayment;
     let mortgagePayment = (Value)*((i*((1+i)**t))/(((1+i)**t)-1));
-    // document.getElementById("Monthly_Mortgage").innerHTML=`The monthly mortgage payment is: ${mortagePayment}`;
-    console.log(mortgagePayment)
-    // return mortgagePayment;
+    let insurance = $("#HomeInsurance").val()/12
+    let taxRate = $("#PropertyTax").val();
+    let taxes = ((taxRate/100)*homePrice)/12;
+    let MonthlyPayment = Math.round(mortgagePayment + insurance + taxes)
+    document.getElementById("Monthly_Mortgage").innerHTML=`The monthly mortgage payment is: $${MonthlyPayment}`;
+    console.log(MonthlyPayment)
+    makeGraph();
+    return MonthlyPayment;
+}
+
+function formatOption(){
+    let term = getSelectedOption("SelectTerm");
+    let answer = term.value;
+    let finalAnswer = parseInt(answer)
+    console.log(term);
+    console.log(answer);
+    console.log(typeof answer);
+    console.log(typeof finalAnswer)
+    return finalAnswer
+}
+function getSelectedOption(selectID){
+    let htmlElement = document.getElementById(selectID);
+    for (let i = 0; i < htmlElement.options.length; i++){
+        let option = htmlElement.options[i];
+        if(option.selected == true){
+            return option;
+        }
+    }
+    return null
+}
+function makeGraph(){
+    let homePrice = $("#HomePrice").val()
+    let downPayment = $("#DownPayment").val();
+    let interestRate = $("#Interest").val();
+    let mortgagePeriod = formatOption();
+    let i = (interestRate/100)/12;
+    let t = mortgagePeriod*12;
+    let Value = homePrice - downPayment;
+    let mortgagePayment = Math.round((Value)*((i*((1+i)**t))/(((1+i)**t)-1)));
+    let insurance = Math.round($("#HomeInsurance").val()/12)
+    let taxRate = $("#PropertyTax").val();
+    let taxes = Math.round(((taxRate/100)*homePrice)/12);
+    console.log("hi")
+    let graph_values = [mortgagePayment, insurance, taxes]
+    let graph_labels = ["Mortgage", "Homeonwer's Insurance", "Property Taxes"]
+    let trace = {
+        values: graph_values,
+        labels: graph_labels,
+        name: "Monthly Payment Breakdown",
+        hoverinfo: 'value+label',
+        hole: .4,
+        type: "pie"
+    }
+    var graphdata= [trace]
+    Plotly.newPlot("piechart",graphdata)
+}
 
 
-// }
-// MortgageCalculator();
 
-// function getSelectedOption(selectID){
-//     let htmlElement = document.getElementById(selectID);
-//     for (let i = 0; i < htmlElement.options.length; i++){
-//         let option = htmlElement.options[i];
-//         if(option.selected == true){
-//             return option;
-//         }
-//     }
-//     return null
-// }
-
-
-// d3.select("#MortgageCalc_Button").on("click", MortgageCalculator)
+d3.select("#MortgageCalc_Button").on("click", MortgageCalculator)
